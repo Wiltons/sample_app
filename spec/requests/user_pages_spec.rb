01bb25a8a2a9414d8bdc4d3a0741358306e349e6
Cillian <cillians@gmail.com>
2014-04-05 00:19:23 +0100

    Additional tweaks to user signup section

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,7 +17,7 @@ describe "User Pages" do
 
       describe "after submission" do
         before { click_button submit }
-
+     
         it { should have_title('Sign up') }
         it { should have_content('error') }
       end
@@ -32,19 +32,27 @@ describe "User Pages" do
       end
 
       it "should create a user" do
-        expect { click_button submit}.to change(User, :count).by(1)
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      it { should have_content('Sign up') }
+      it { should have_title(full_title('Sign up')) }
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+  
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
-  end
+    describe "profile page" do
+      let(:user) { FactoryGirl.create(:user) }    
+      before { visit user_path(user) }
 
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }    
-    before { visit user_path(user) }
-
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
+      it { should have_content(user.name) }
+      it { should have_title(user.name) }
+    end
   end
 end
