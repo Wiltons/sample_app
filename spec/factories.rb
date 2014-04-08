22cbfb684b9403ada40843235f9a0037c2441602
Cillian <cillians@gmail.com>
2014-04-08 14:05:27 +0100

    Added fakers for generating test data. Paginated user index screen

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name	"Cillian Murphy"
-    email	"cillians@test.com"
+    sequence(:name)   { |n| "Person_#{n}"}
+    sequence(:email)  { |n| "Person_#{n}@example.com"}
     password	"foobar"
     password_confirmation "foobar"
   end
