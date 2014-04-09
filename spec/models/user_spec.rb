11d7f4c4d5d44f21bbfc0eae88b0f4b0398a048b
Cillian <cillians@gmail.com>
2014-04-09 14:02:47 +0100

    Small refactoring and added admin attribute

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -68,6 +68,7 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
   it { should be_valid }
 
   describe "when password is not present" do
@@ -112,5 +113,14 @@ describe User do
       @user.save
       expect(@user.reload.email).to eq mixed_case_email.downcase
     end
+  end
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it {should be_admin}
   end
 end
