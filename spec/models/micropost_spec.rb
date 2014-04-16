0f1deadf400dc28eed8a24ebf63b24abc2340e75
Cillian <cillians@gmail.com>
2014-04-16 14:13:33 +0100

    Microposts implementation, updated home page, added tests

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Micropost do
   let(:user) {FactoryGirl.create(:user)}
 
-  before {user.microposts.build(content:"Lorem Ipsum")}
+  before {@micropost=user.microposts.build(content:"Lorem Ipsum")}
 
   subject {@micropost}
 
@@ -16,6 +16,16 @@ describe Micropost do
 
   describe "when user_id is not present" do
     before {@micropost.user_id=nil}
+    it {should_not be_valid}
+  end
+
+  describe "with blank content" do
+    before {@micropost.content=" "}
+    it {should_not be_valid}
+  end
+
+  describe "with content that is too long" do
+    before{@micropost.content="a"*141}
     it {should_not be_valid}
   end
 end
